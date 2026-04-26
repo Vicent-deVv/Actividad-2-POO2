f570ff6e3a4a6bb816b424546ca2755f40f53648
Vicent-deVv <175661774+Vicent-deVv@users.noreply.github.com>
2026-04-26 14:04:29 -0500

    Historia de usuario 1 completada

--- src/FacturaAdapter.java
+++ src/FacturaAdapter.java
@@ -1,7 +1,17 @@
 
 public class FacturaAdapter implements FacturaService {
+    private LegacyBillingSystem legacy;
+
+    public FacturaAdapter(LegacyBillingSystem legacy){
+        this.legacy = legacy;
+    }
+
     @Override
     public void emitirFactura(double Monto, String cliente) {
+        legacy.abrirConeccion();
 
+        legacy.generarFactura(cliente,Monto);
+
+        System.out.println("Factura generada con exito para el cliente:" + cliente + " por un monto de:" + Monto);
     }
 }
--- src/LegacyBillingSystem.java
+++ src/LegacyBillingSystem.java
@@ -1,9 +1,10 @@
 public class LegacyBillingSystem {
+    //Metodos Meramente Referenciales
     public void abrirConeccion(){
-        System.out.println("Abriendo Coneccion");
+        System.out.println("[Legacy]Abriendo Coneccion");
     }
 
     public void generarFactura(String nombre, double val){
-
+        System.out.println("[Legacy]Creando registro para: " + nombre + " con valor: " + val);
     }
 }
